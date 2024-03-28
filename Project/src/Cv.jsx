2fb2511670b38ffab0079beab4f87259e9d81354
Ilkka Korhonen <99470756+ilkkaKorhonenFuturiot@users.noreply.github.com>
2024-03-28 18:43:53 +0200

    react fix for deploy 3

--- Project/src/Cv.jsx
+++ Project/src/Cv.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
+import myCv from "./assets/CV_Korhonen_Ilkka.pdf";
 
 const Cv = () => (<main id="home">
     <h1 className="lg-heading">Curriculum Vitae of <span className="text-secondary">Ilkka</span> Korhonen</h1>
-    <p><Link to="/CV_Korhonen_Ilkka.pdf">Download here</Link></p>
+    <p><Link to={myCv}>Download here</Link></p>
 
 </main>
 
