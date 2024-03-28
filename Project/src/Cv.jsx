76e42dd1b8e4968e6509c421c987cfe9cabb0797
Ilkka Korhonen <99470756+ilkkaKorhonenFuturiot@users.noreply.github.com>
2024-03-28 18:56:41 +0200

    react fix for deploy 5

--- Project/src/Cv.jsx
+++ Project/src/Cv.jsx
@@ -3,7 +3,7 @@ const myCv = new URL('./assets/CV_Korhonen_Ilkka.pdf', import.meta.url).href
 
 const Cv = () => (<main id="home">
     <h1 className="lg-heading">Curriculum Vitae of <span className="text-secondary">Ilkka</span> Korhonen</h1>
-    <p><Link to={myCv}>Download here</Link></p>
+    <p><a href={myCv}>Download here</a></p>
 
 </main>
 
