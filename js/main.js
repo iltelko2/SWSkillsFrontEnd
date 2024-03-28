17d1558c95567ef45fa5fd29f04e6becf76e4b86
Ilkka Korhonen <99470756+ilkkaKorhonenFuturiot@users.noreply.github.com>
2024-03-28 09:26:39 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,32 +1,54 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuPortrait = document.querySelector('.menu-portrait');
 
 const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.remove('hide'));
 
         navItems.forEach(item => item.classList.add('show'));
 
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        sleep(200).then(() => {
+            menuBtn.classList.remove('open');
+            menu.classList.remove('hide');
+            menuNav.classList.remove('hide');
+            menuPortrait.classList.remove('hide');
 
+            menuBtn.classList.add('close');
+            menu.classList.add('show');
+            menuNav.classList.add('show');
+            menuPortrait.classList.add('show');
+
+            showMenu = true;
+        });
+    } else {
         navItems.forEach(item => item.classList.remove('show'));
 
-        showMenu = false;
+        navItems.forEach(item => item.classList.add('hide'));
+
+        sleep(200).then(() => {
+            menuBtn.classList.remove('close');
+            menu.classList.remove('show');
+            menuNav.classList.remove('show');
+            menuPortrait.classList.remove('show');
+
+            menuBtn.classList.add('open');
+            menu.classList.add('hide');
+            menuNav.classList.add('hide');
+            menuPortrait.classList.add('hide');
+
+            showMenu = false;
+        });
     }
 }
