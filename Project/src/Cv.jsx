df5d6862f0a4c4fb5f2fe68142de251078d798ec
Ilkka Korhonen <99470756+ilkkaKorhonenFuturiot@users.noreply.github.com>
2024-03-28 19:11:12 +0200

    react fix for deploy 7

--- Project/src/Cv.jsx
+++ Project/src/Cv.jsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom"
 const myCv = new URL('./assets/CV_Korhonen_Ilkka.pdf', import.meta.url).href
+
+const onClick = () => {
+
+    fetch(myCv).then((response) => {
+        response.blob().then((blob) => {
+            const fileURL = window.URL.createObjectURL(blob);
+            let alink = document.createElement("a");
+            alink.href = fileURL;
+            alink.download = "CV_Korhonen_Ilkka.pdf";
+            alink.click();
+        });
+    });
+};
 
 const Cv = () => (<main id="home">
     <h1 className="lg-heading">Curriculum Vitae of <span className="text-secondary">Ilkka</span> Korhonen</h1>
-    <p><a href={myCv}>Download here</a></p>
+    <p><button onClick={onClick}>Download here</button></p>
 
 </main>
 
